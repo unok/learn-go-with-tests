dbffe0eec7e2c481d59a2a444f33eb16422f8a5e
Kazuhiko UNO <kazuhiko.uno@gmail.com>
2021-09-02 21:47:54 +0900

    Hello world まで完了

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,28 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		return frenchHelloPrefix
+	case spanish:
+		return spanishHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
+}
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
